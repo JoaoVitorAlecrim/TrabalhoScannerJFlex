cbb31d4e752016780c1c0acf7ca3b056fff0930f
Thiago Kenji Ikuta <thikutakenji@gmail.com>
2026-04-21 21:46:40 -0300

    Add files via upload

--- trabalho1-compiladores/Main.java
+++ trabalho1-compiladores/Main.java
@@ -5,13 +5,13 @@ public class Main {
         try {
             /*
              * Se o usuário passar um nome de arquivo, usa esse arquivo.
-             * Caso contrário, usa "entrada.txt" como padrão.
+             * Caso contrário, usa "teste_completo.txt" como padrão.
              */
             String nomeArquivo;
             if (args.length > 0) {
                 nomeArquivo = args[0];
             } else {
-                nomeArquivo = "entrada.txt";
+                nomeArquivo = "teste_completo.txt";
             }
 
             Scanner scanner = new Scanner(new FileReader(nomeArquivo));
